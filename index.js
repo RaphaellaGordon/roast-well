aa7dbdd758d3f51b3394e8384357a405d1caf573
raphaellagordon <raphaella.florence@gmail.com>
2019-10-18 17:04:00 +0100

    attempting to serve app to heroku without display db credentials

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const ordersRouter = require('./routes/orders');
 const productsRouter = require('./routes/products');
 const cartsRouter = require('./routes/carts');
 const path = require('path');
-// import dotenv from 'dotenv';
 require('dotenv').config({ path: '.ENV' });
 
 const app = express();
@@ -17,10 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-// MONGODB= 'mongodb://user:password123@ds137008.mlab.com:37008/heroku_n09zfqnh';
-// MONGODB_URI= 'mongodb://user:password123@ds137008.mlab.com:37008/heroku_n09zfqnh';
-// MONGODB_URI= 'mongodb://heroku_n09zfqnh:cvek1cs7ju6j8a7nq64d9hv2dq@ds137008.mlab.com:37008/heroku_n09zfqnh';
+
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('My MONGODB_URI is ' + process.env.MONGODB_URI))
 .catch(err => console.log(err));
